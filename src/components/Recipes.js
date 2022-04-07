2a46b09dbabda8c3103720d821f2c425060f03ea
Rene2022 <renatescherwath@gmail.com>
2022-04-07 17:37:12 +0200

    saved changes to creat branch

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -3,21 +3,25 @@ import { useNavigate } from "react-router-dom";
 export default function Recipes(props) {
   const navigate = useNavigate();
   return (
-    <div className="bottom-pink-border col-sm-5 col-md-3 mx-1 my-5 p-2 card card-custom shadow-lg">
-      <img src={props.img} className="img-fluid" alt={props.title}></img>
-      <div className="card-body d-flex flex-column text-center">
-        <h5 className="">{props.title}</h5>
+    
+    
+      <div className="bottom-pink-border col-sm-5 col-md-3 mx-1 my-5 p-2 card card-custom shadow-lg">
+        
+        <img src={props.img} className="img-fluid" alt={props.title}></img>
+        <div className="card-body d-flex flex-column text-center">
+          <h5 className="">{props.title}</h5>
+        </div>
+        <div className="text-center">
+          <small
+            className="btn btn-outline-dark py-1 px-5"
+            onClick={() => {
+              navigate(`/recipe/${props.id}`);
+            }}
+          >
+            VIEW RECIPE
+          </small>
+        </div>
       </div>
-      <div className="text-center">
-        <small
-          className="btn btn-outline-dark py-1 px-5"
-          onClick={() => {
-            navigate(`/recipe/${props.id}`);
-          }}
-        >
-          VIEW RECIPE
-        </small>
-      </div>
-    </div>
+    
   );
 }
